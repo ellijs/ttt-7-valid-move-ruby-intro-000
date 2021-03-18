8a466097e489d91a2872beccb1cb8191205c7f16
ji sun lee <ellijs@hotmail.com>
2021-03-18 01:25:39 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move(board, index)
   if (position_taken(board, index) == false && index >= 0 && index <= 8)
     return true
-  elsif 
+  elsif
     position_taken(board,index) == true
     return false
   else
